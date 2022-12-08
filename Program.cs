464580b67523a2ff2572746d930867c9313ae2e2
Daniel Guirra <daniel.guirra777@gmail.com>
2022-12-07 23:07:48 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Curse
+{
+   public class Program
+   {
+
+      static void Main()
+      {
+         System.Console.WriteLine("Calcular valor maior de tres valores");
+         System.Console.WriteLine("Digite os tres valores");
+
+
+         int x = int.Parse(Console.ReadLine());
+         int y = int.Parse(Console.ReadLine());
+         int z = int.Parse(Console.ReadLine());
+
+         int result = CalcularMaior(x, y, z);
+
+         System.Console.WriteLine("O maior valor é : " + result);
+
+      }
+
+      static int CalcularMaior(int a, int b, int c)
+      {
+         if (a > b && a > c)
+         {
+            return a;
+         }
+         else if (b > c)
+         {
+            return b;
+         }
+         else
+         {
+            return c;
+         }
+      }
+
+
+   }
+}
