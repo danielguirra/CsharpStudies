83d60bc94f2b6ac2bfce645652ab7c3c7834a706
Daniel Guirra <daniel.guirra777@gmail.com>
2022-12-07 23:12:08 -0300

    more projects

---
+++ console-write/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace ConsoleWriteAtividade
+{
+   public class Program
+   {
+      static void Main()
+      {
+         string produto1 = "Computador";
+         string produto2 = "Mesa de escritório";
+
+         byte idade = 30;
+         int codigo = 5290;
+         char genero = 'M';
+
+
+         double preco1 = 2100.0;
+         double preco2 = 650.50;
+         double medida = 53.234567;
+
+         System.Console.WriteLine("Produtos");
+         System.Console.WriteLine($"{produto1}, cujo o preço é $ {preco1:f2}");
+         System.Console.WriteLine($"{produto2}, cujo o preço é $ {preco2:f2}");
+         System.Console.WriteLine($"\r\n");
+         System.Console.WriteLine($"Registro: {idade}, código {codigo} e gênero: {genero}");
+         System.Console.WriteLine($"\r\n");
+         System.Console.WriteLine($"Medida com oito casas decimais: {medida}");
+         System.Console.WriteLine($"Medida com três casas decimais: {medida:F3}");
+         System.Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+
+      }
+   }
+}
---
+++ equacao2grau/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Program
+{
+   public class Program
+   {
+      static void Main()
+      {
+         Console.WriteLine("Calcula Equação do 2Grau");
+
+         System.Console.WriteLine("Determine o valor de A:");
+         int a = Int16.Parse(System.Console.ReadLine());
+
+         System.Console.WriteLine("Determine o valor de B:");
+         int b = Int16.Parse(System.Console.ReadLine());
+
+         System.Console.WriteLine("Determine o valor de C:");
+         int c = Int16.Parse(System.Console.ReadLine());
+
+         double delta = Math.Pow(b, 2) - 4 * a * c;
+
+         double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+         double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+         System.Console.WriteLine("x1: " + x1);
+         System.Console.WriteLine("x2: " + x2);
+
+      }
+   }
+}
