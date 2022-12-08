85f16752dc1e64b49d02620dd9ec1767e6df0c93
Daniel Guirra <daniel.guirra777@gmail.com>
2022-12-07 23:44:52 -0300

    while

---
+++ while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System;
+
+namespace While
+{
+   public class Program
+   {
+      static void Main()
+      {
+         Console.WriteLine("Calcula raiz quadrada");
+         Console.Write("Digite um numero :");
+
+         double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+         while (x > 0)
+         {
+
+            double z = Math.Sqrt(x);
+            System.Console.WriteLine($"Raiz do numero é :{z:F3}");
+            Console.Write("Digite um novo numero :");
+            x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+         }
+
+         System.Console.WriteLine("O valor a ser calculado não pode ser negativo");
+      }
+   }
+}
