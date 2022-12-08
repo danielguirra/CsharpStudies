cd06d5d701771db99bbdd53b5fc96e18b0298fef
Daniel Guirra <daniel.guirra777@gmail.com>
2022-12-07 23:54:37 -0300

    for

---
+++ for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace For
+{
+   public class Program
+   {
+      static void Main()
+      {
+         Console.Write("Quantos numero inteiros voce vai digitar ? :");
+         int valueForLoop = int.Parse(Console.ReadLine());
+         int soma = 0;
+
+         for (int i = 0; i < valueForLoop; i++)
+         {
+            Console.Write($"Digite o {i + 1}:");
+            int valueToSome = int.Parse(Console.ReadLine());
+            soma = soma + valueToSome;
+         }
+         Console.WriteLine("SOMA: " + soma);
+      }
+   }
+}
